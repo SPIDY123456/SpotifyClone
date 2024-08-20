6dba91057c3dcdee41b927e222f502a3b689c237
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-08-20 09:54:22 +0530

    Add files via upload

---
+++ src/API/authorize.tsx
@@ -0,0 +1,120 @@
+
+const SPOTIFY_CLIENT_ID: string = `b3fc46a61fa94c8781550156ed0a9b50`;
+//const SPOTIFY_CLIENT_SECRET_ID = `0f8f9d874cef434a96172b6bb8f6835e`;
+const redirectUri: string = `http://localhost:3000`;
+
+
+
+function generateRandomString(length: number): string {
+    let text = "";
+    const possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string): Promise<string> {
+    function base64encode(string: Uint8Array): string {
+        return btoa(String.fromCharCode.apply(null, Array.from(string)))
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_")
+            .replace(/=+$/, "");
+    }
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+    return base64encode(new Uint8Array(digest));
+}
+
+const codeVerifier: string = generateRandomString(128);
+
+let urlParams = new URLSearchParams();
+
+if (typeof window !== "undefined") {
+    urlParams = new URLSearchParams(window.location.search);
+}
+
+export const authorize =  () => {
+    generateCodeChallenge(codeVerifier).then((codeChallenge) => {
+        const state: string = generateRandomString(16);
+        const scope: string =
+            "user-read-private user-read-email streaming user-read-playback-state user-modify-playback-state";
+
+        sessionStorage.setItem("code_verifier", codeVerifier);
+
+        const args = new URLSearchParams({
+            response_type: "code",
+            client_id: SPOTIFY_CLIENT_ID,
+            scope: scope,
+            redirect_uri: redirectUri,
+            state: state,
+            code_challenge_method: "S256",
+            code_challenge: codeChallenge,
+        });
+
+        window.location.href = "https://accounts.spotify.com/authorize?" + args;
+    });
+
+    
+};
+
+    export const getToken = () => {
+    const codeVerifier = sessionStorage.getItem("code_verifier");
+
+    const  body = new URLSearchParams({
+        grant_type: "authorization_code" || "",
+        code: urlParams.get("code") || "",
+        redirect_uri: redirectUri || "",
+        client_id: SPOTIFY_CLIENT_ID || "",
+        code_verifier: codeVerifier || "",
+    });
+            fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: body,
+        })
+
+//         return response.json();
+//     } catch (error) {
+//         window.location.href = "/";
+//     }
+// };
+
+//  const refreshSpotifyToken = async (refresh_token: string) => {
+//     const body = new URLSearchParams({
+//         grant_type: "refresh_token" || "",
+//         refresh_token: refresh_token,
+//         client_id: SPOTIFY_CLIENT_ID || "",
+//     });
+//            const response = await fetch("https://accounts.spotify.com/api/token", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/x-www-form-urlencoded",
+//             },
+//             body:body,
+        //})
+        .then((response) => {
+
+            if (!response.ok) {
+                throw new Error("HTTP Status " + response.status);           
+        }
+
+        return response.json();
+    })
+    .then((data:{access_token: string}) => {
+        sessionStorage.setItem("access_token", data.access_token);
+        
+    })
+
+
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+};
---
+++ src/components/Player/index.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect,useState } from 'react'
+import { getTracks } from '~/API/searchTrack';
+import styles from "../Player/Player.module.css"; 
+import SpotifyPlayer from 'react-spotify-web-playback';
+
+
+
+
+
+const Player = () => {
+
+
+   
+    const[searchInput,setSearchInput] = useState("");
+    const[searchResults,setSearchResults] = useState([]);
+    const[currentTrack,setCurrentTrack] = useState("");
+    const[access_token,setAccessToken] = useState("");
+    const getCurrentTrack = (currentTrackURI: string) => {
+
+         setCurrentTrack(currentTrackURI);
+    }
+
+ 
+
+    
+    const fetchTracks = async () => {
+    if(searchInput) {
+      let response = await getTracks(searchInput);
+      setSearchResults(response.tracks.items.map((track : {
+        name : "";
+        uri: "";
+       album : {
+       name : "";
+        images : [{
+        url:"";
+       },
+       {
+        url:"";
+       }
+      ];
+     }; 
+     
+      }) => {
+
+      return {
+        name :track.name,
+        uri :track.uri,
+        images: track.album.images[1].url,
+        albumName :track.album.name,
+
+      }
+      }) || {});
+    }
+
+    };
+
+    useEffect(() => {
+        let debounced =setTimeout(() => {
+        fetchTracks();
+
+        },100);
+
+
+     return  () => clearTimeout(debounced); 
+
+    },[searchInput]);
+
+    useEffect(() => {
+      let token = sessionStorage.getItem("access_token");
+      setAccessToken(token || "");
+    },[])
+    console.log(access_token);
+  return (
+    <div className={styles.her}>
+        <input type="text" placeholder="Search a Track..." 
+        value={searchInput}
+        onChange={(event) => {
+            setSearchInput(event.target.value);
+        }}
+        className={`input input-bordered w-full max-w-xs ${styles.input}`} 
+        />
+        <div className={styles.grid}>
+         {searchResults.map((track: { name : "" ; images:" " ;uri:""}) => { 
+            return (
+              <div className={styles.cover} onClick={() => getCurrentTrack(track.uri)}>
+                <img className= {styles.trackImage} src ={track.images}/>
+                <p className={styles.tracks}>{track.name}</p>
+                
+                  {/* <button className={`btn btn-accent ${styles.playbtn}`}> Play </button> */}
+                </div>
+            )
+          })}
+          
+        </div>
+
+     
+        <div className={styles.spotifyPlayer}>
+        {access_token ? (
+        <SpotifyPlayer
+          token={access_token}
+          play
+          styles ={{
+            bgColor: '#333',
+            color: '#fff',
+            loaderColor: '#fff',
+            sliderColor: '#1cb954',
+            sliderHandleColor:'whitesmoke',
+            trackArtistColor: '#ccc',
+            trackNameColor: '#fff',
+
+          }}
+          uris={[currentTrack]}
+          // uris={searchResults.map((item: {uri : ""}) => item.uri)}  
+          
+        />
+          ):(
+          <></>
+        )}    
+        </div>
+    </div>
+  );
+}
+
+
+export default Player;
